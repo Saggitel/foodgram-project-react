1849b4b1e3f7d8eeae9565e724df31382281e7ef
Тимур Лазба <lazba.timur@gmail.com>
2023-04-09 01:21:01 +0300

    flake8

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -7,12 +7,12 @@ class IsOwnerOrAdminOrReadOnly(permissions.BasePermission):
        Неавторизованным пользователям разрешён только просмотр.
        Владельцу и админу доступны все методы.'''
     def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated)
+        return (request.method 
+                in permissions.SAFE_METHODS or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or obj.author == request.user)
+        return (request.method in 
+                permissions.SAFE_METHODS or obj.author == request.user)
 
 
 class IsCurrentUserOrAdminOrReadOnly(permissions.BasePermission):
@@ -20,5 +20,5 @@ class IsCurrentUserOrAdminOrReadOnly(permissions.BasePermission):
        Неавторизованным пользователям разрешён только просмотр.
        Пользователю и админу доступны все методы.'''
     def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or obj.id == request.user)
+        return (request.method in 
+                permissions.SAFE_METHODS or obj.id == request.user)
